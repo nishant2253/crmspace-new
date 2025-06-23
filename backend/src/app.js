ab296cea77f5a502a2b5319c878a6ad0bf715e34
nishant gupta <nishantgupta2253@gmail.com>
2025-06-23 21:49:13 +0530

    updated bydefault port in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -98,7 +98,7 @@ app.use(
 
 app.use(router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
